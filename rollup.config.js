f4ec1ecee4d15373bde4b7e062e416c79890cb9a
ohager <oliver@devbutze.com>
2023-06-08 16:05:13 -0300

    added bundle script, and fixed issue with group chat on bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import svelte from "rollup-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
-import { vitePreprocess } from '@sveltejs/kit/vite';
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import nodePolyfills from 'rollup-plugin-polyfill-node';
@@ -11,15 +10,13 @@ import serve from "rollup-plugin-serve";
 import json from "@rollup/plugin-json";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
-import esformatter from "rollup-plugin-esformatter";
-import postcss from "rollup-plugin-postcss";
 
-const production = true; //false;// !process.env.ROLLUP_WATCH;
+const production = process.env.NODE_ENV==="production";
 
 export default {
 	input: "src/widget.js",
 	output: {
-		file: "static/public/bundle.js",
+		file: "public/bundle.js",
 		format: "iife",
 		name: "app",
 		sourcemap: production,
@@ -40,7 +37,6 @@ export default {
 						plugins: [tailwindcss(), autoprefixer()],
 					},
 				}),
-				vitePreprocess(),
 			],
 		}),
 
@@ -56,16 +52,6 @@ export default {
 
 		// export CSS in separate file for better performance
 		css({ output: "bundle.css" }),
-		// postcss({
-		// 	config: {
-		// 		path: "./postcss.config.js",
-		// 	},
-		// 	extensions: [".css"],
-		// 	minimize: true,
-		// 	inject: {
-		// 		insertAt: "top",
-		// 	},
-		// }),
 
 		// start a local livereload server on public/ folder
 		!production && serve("public/"),
